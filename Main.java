44c3ec1594756ce2dcba745cece0722b0ef86ef1
Viktorija Gorbunova <vika_gorbunova_2014@mail.ru>
2022-12-12 19:37:36 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       int  amount = 1000;
+       int wallet = 50;
+
+       int percent;
+        if (amount >= 1000) {
+
+            percent=amount/100;
+            System.out.println("Итоговая сумма: " + (amount+wallet) + " Начисенно бонусов " + percent);
+        }
+         else {
+            System.out.println("Итоговая сумма:" + (amount+wallet) + "  Бонусов не начисено" );
+        }
+
+    }
+
+}
