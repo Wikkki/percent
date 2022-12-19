9caccda180e266177df9a3b4f7e48d127589edce
Wikkki <108623629+Wikkki@users.noreply.github.com>
2022-12-19 21:31:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-       int  amount = 1000;
-       int wallet = 50;
+        intamount = 1000;
+        int wallet = 50;
 
-       int percent;
+        int percent;
         if (amount >= 1000) {
 
-            percent=amount/100;
-            System.out.println("Итоговая сумма: " + (amount+wallet) + " Начисенно бонусов " + percent);
-        }
-         else {
-            System.out.println("Итоговая сумма:" + (amount+wallet) + "  Бонусов не начисено" );
+            percent = amount / 100;
+            System.out.println("Итоговая сумма: " + (amount + wallet) + " Начисенно бонусов " + percent);
+        } else {
+            System.out.println("Итоговая сумма:" + (amount + wallet) + " Бонусов не начисено");
         }
 
     }
